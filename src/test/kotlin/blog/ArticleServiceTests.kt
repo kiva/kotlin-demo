1335332afa611dc2293e5a7c64d94bd916be09c9
Leo Perry <leop@kiva.org>
2018-08-17 16:39:15 -0700

    Mocked out repository dependencies in ArticleService unit tests

--- src/test/kotlin/blog/ArticleServiceTests.kt
+++ src/test/kotlin/blog/ArticleServiceTests.kt
@@ -6,24 +6,53 @@ import org.springframework.boot.test.context.SpringBootTest
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.junit.jupiter.api.Assertions.assertEquals
+import org.springframework.boot.test.mock.mockito.MockBean
+import org.mockito.BDDMockito.given
+import java.util.Optional
+import org.mockito.BDDMockito.any
+import org.mockito.BDDMockito.verify
+import org.mockito.BDDMockito.argThat
 
 @ExtendWith(SpringExtension::class)
 @SpringBootTest
 class ArticleServiceTests(@Autowired val service: ArticleService) {
 
+    @MockBean
+    private lateinit var articleRepository: ArticleRepository
+
+    @MockBean
+    private lateinit var userRepository: UserRepository
+
     @Test
-    fun `Assert getArticle(id) returns null on nonexistent ID`() {
-        val nonExistentId = 3141592654
-        val value = service.getArticle(nonExistentId)
-        assertEquals(null, value)
+    fun `Assert getArticle(id) returns an article if ID exists`() {
+        val testId: Long = 1
+        val author = User("testlogin", "First", "Last")
+        val testArticle = Article("test title", "test content", author)
+
+        given(articleRepository.findById(testId)).willReturn(Optional.ofNullable(testArticle))
+
+        val fetched = service.getArticle(testId)
+
+        assertEquals(testArticle, fetched)
+        verify(articleRepository).findById(testId)
     }
 
     @Test
     fun `Assert saveArticle() saves an article`() {
-        val title = "Snappy Title"
-        val content = "This is a great article"
-        val saved = service.saveArticle(title, content)
-        assertEquals(title, saved.title)
-        assertEquals(content, saved.content)
+        val testTitle = "Snappy Title"
+        val testContent = "This is a great article"
+        val author = User("testlogin", "First", "Last")
+        val testArticle = Article(testTitle, testContent, author)
+
+        given(userRepository.save(any<User>())).willReturn(author)
+        given(articleRepository.save(any<Article>())).willReturn(testArticle)
+
+        val saved = service.saveArticle(testTitle, testContent)
+
+        assertEquals(testTitle, saved.title)
+        assertEquals(testContent, saved.content)
+        verify(articleRepository).save(argThat {article ->
+            (article.title == testTitle && article.content == testContent)
+        })
     }
 }
